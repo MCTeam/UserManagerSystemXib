a05b34b9c21c2972c5ff5161f35c18c8094f7b63
yellow10 <yellow.10@qq.com>
2013-01-18 15:37:54 +0000

    user system with xib
    add personal score table to show the top 5 score of current user..but
    there're some bug with the segmented control button. After switching
    several times the app will crash.

--- UserManagerSystemXib/UserManagerSystemViewController.h
+++ UserManagerSystemXib/UserManagerSystemViewController.h
@@ -7,7 +7,37 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "MCUserManagerController.h"
+#import "PopCreateUserViewController.h"
+#import "PopChangeUserViewController.h"
+#import "ScoreCell.h"
 
 @interface UserManagerSystemViewController : UIViewController
+    <UITableViewDelegate, UITableViewDataSource, UIPopoverControllerDelegate>
+{
+    MCUserManagerController *userManagerController;
+}
 
+@property (retain, nonatomic) IBOutlet UITableView *scoreTable;
+
+@property (retain, nonatomic) IBOutlet UITextField *insertScoreTimeField;
+@property (retain, nonatomic) IBOutlet UITextField *insertScoreMoveField;
+
+@property (retain, nonatomic) IBOutlet UILabel *currentUserLabel;
+
+@property (retain, nonatomic) IBOutlet UILabel *totalGamesLabel;
+@property (retain, nonatomic) IBOutlet UILabel *totalTimesLabel;
+@property (retain, nonatomic) IBOutlet UILabel *totalMovesLabel;
+
+@property (retain, nonatomic) UIPopoverController *createUserPopover;
+@property (retain, nonatomic) UIPopoverController *changeUserPopover;
+
+@property (retain, nonatomic) IBOutlet UISegmentedControl *tableSegment;
+
+- (IBAction)createUserPress:(id)sender;
+- (IBAction)changeUserPress:(id)sender;
+- (IBAction)insertScorePress:(id)sender;
+- (void)updateUserInformation;
+- (void)updateScoreInformation;
+- (IBAction)segmentChange:(id)sender;
 @end
